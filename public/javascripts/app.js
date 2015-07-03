5ce5c6c1659bff9b903155593ffcd566a4def7c4
Gabe Martinez <mgabemartinez@gmail.com>
2015-07-02 22:47:09 -0700

    Changed slider to update (music location) date on change.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -312,11 +312,11 @@ var updateSeekPercentage = function($seekBar, event) {
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Traveler', length: '4:36', audioUrl: '/music/placeholders/Traveler' }, 
-       { name: 'Dreamreader', length: '4:26', audioUrl: '/music/placeholders/Dreamreader' },
-       { name: 'Mala', length: '4:36', audioUrl: '/music/placeholders/Mala' },
-       { name: 'Gold', length: '6:22', audioUrl: '/music/placeholders/Gold' },
-       { name: 'Above', length: '7:06', audioUrl: '/music/placeholders/Above' }
+      { name: 'Traveler', length: 263.38, audioUrl: '/music/placeholders/Traveler' }, 
+      { name: 'Dreamreader', length: 205.66, audioUrl: '/music/placeholders/Dreamreader' },
+      { name: 'Mala', length: 370.14, audioUrl: '/music/placeholders/Mala' },
+      { name: 'Gold', length: 254.81, audioUrl: '/music/placeholders/Gold' },
+      { name: 'Above', length: 475.92, audioUrl: '/music/placeholders/Above' }
      ]
  };
 
@@ -482,6 +482,14 @@ blocJams.service('SongPlayer', function() {
       this.setSong(this.currentAlbum, song);
      },
 
+    seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+      if(currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+        currentSoundFile.setTime(time);
+      }
+    },
+
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -505,56 +513,90 @@ blocJams.directive('slider', ['$document', function($document){
 
   // Returns a number between 0 and 1 to determine where the mouse event happened along the slider bar.
   var calculateSliderPercentFromMouseEvent = function($slider, event) {
-    var offsetX =  event.pageX - $slider.offset().left; // Distance from left
-    var sliderWidth = $slider.width(); // Width of slider
-    var offsetXPercent = (offsetX  / sliderWidth);
-    offsetXPercent = Math.max(0, offsetXPercent);
-    offsetXPercent = Math.min(1, offsetXPercent);
-    return offsetXPercent;
-  } 
+     var offsetX =  event.pageX - $slider.offset().left; // Distance from left
+     var sliderWidth = $slider.width(); // Width of slider
+     var offsetXPercent = (offsetX  / sliderWidth);
+     offsetXPercent = Math.max(0, offsetXPercent);
+     offsetXPercent = Math.min(1, offsetXPercent);
+     return offsetXPercent;
+  }
+   
+  var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+  }
 
   return {
     templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
     replace: true,
     restrict: 'E',
-    scope: {},
+    scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
       var $seekBar = $(element);
+
+      attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+ 
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
  
       var percentString = function () {
-        var percent = Number(scope.value) / Number(scope.max) * 100;
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + "%";
       }
  
       scope.fillStyle = function() {
-        return {width: percentString()};
+         return {width: percentString()};
       }
  
       scope.thumbStyle = function() {
-        return {left: percentString()};
+         return {left: percentString()};
       }
 
       scope.onClickSlider = function(event) {
         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
         scope.value = percent * scope.max;
+        notifyCallback(scope.value);
       }
 
       scope.trackThumb = function() {
-         $document.bind('mousemove.thumb', function(event){
-           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-           scope.$apply(function(){
-             scope.value = percent * scope.max;
-           });
-         });
+        $document.bind('mousemove.thumb', function(event) {
+          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+          scope.$apply(function() {
+            scope.value = percent * scope.max;
+            notifyCallback(scope.value);
+          });
+        });
  
          //cleanup
-         $document.bind('mouseup.thumb', function(){
-           $document.unbind('mousemove.thumb');
-           $document.unbind('mouseup.thumb');
-         });
-       };
+        $document.bind('mouseup.thumb', function(){
+          $document.unbind('mousemove.thumb');
+          $document.unbind('mouseup.thumb');
+        });
+      };  
+
+      var notifyCallback = function(newValue) {
+        if(typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
+      }; 
     }
   };
 }]);
